c19453a62a9a247ed4def76a048c6c1e1a3c73e3
Chandrasekar.Rajagopal <Chandrasekar.Rajagopal@parker.com>
2024-10-22 01:05:50 +0530

    video update

--- components/VideoPlayer.js
+++ components/VideoPlayer.js
@@ -3,34 +3,13 @@ import React from 'react';
 
 const VideoPlayer = () => {
 
-    const handleWatchVideo = () => {
-        // Navigate to the API route that serves the video
-        window.open('/api/video', '_blank');
-      };
-
-      const handleWatchVideoattachment = () => {
-        // Navigate to the API route that serves the video
-        window.open('/api/video1', '_blank');
-      };
-
+    
 
   return (
     <div>
 
-<h1>Watch Video</h1>
-inline
-<video controls width="600">
-        <source src="/api/video" type="video/mp4" /> {/* Use the API route here */}
-        Your browser does not support the video tag.
-      </video>
-      
-      attachment
-<video controls width="600">
-        <source src="/api/video1" type="video/mp4" /> {/* Use the API route here */}
-        Your browser does not support the video tag.
-      </video>
 
-playsInline=============
+playsInline and video from public folder
       <video
         width="100%"
         height="300"
@@ -42,20 +21,11 @@ playsInline=============
         <source src="/videos/HOSETestDrive.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
-      {/* controls| autoPlay
-      <video
-        width="100%"
-        height="300"
-        controls
-        autoPlay
-      >
-        <source src="/videos/HOSETestDrive.mp4" type="video/mp4" />
-        Your browser does not support the video tag.
-      </video> */}
+     
 <br />
 <br />
 <br />
-===video from parker===
+===video from parker /content/dam===
 <video controls autoplay muted class="additional-image-dialog_video__PUwN7" src="https://www.parker.com/content/dam/videos/fcg/fluid-system-connectors-division/installation_videos/core_brass/HOSE_BARB_125HBL-6-6_FINAL.mp4"></video>
 
     </div>

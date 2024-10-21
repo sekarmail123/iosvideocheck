ee8204b0b1418e832b37eb5770ee593cff6ee246
Chandrasekar.Rajagopal <Chandrasekar.Rajagopal@parker.com>
2024-10-22 00:20:18 +0530

    play inline

--- components/VideoPlayer.js
+++ components/VideoPlayer.js
@@ -4,11 +4,24 @@ import React from 'react';
 const VideoPlayer = () => {
   return (
     <div>
+playsInline=============
       <video
         width="100%"
         height="300"
         controls
         playsInline
+        autoPlay
+        muted
+      >
+        <source src="/videos/HOSETestDrive.mp4" type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+
+no playsInline=============
+      <video
+        width="100%"
+        height="300"
+        controls
         autoPlay
         muted
       >
